635ca1f632286e59903f5f37e9e9acf86727763f
martadoberschuetz <marta@marta.ie>
2014-08-28 20:16:02 +0100

    First commit

---
+++ src/main/MainClass.java
@@ -0,0 +1,19 @@
+package main;
+import gui.MainFrame;
+
+
+
+public class MainClass {
+	
+	public static void main(String[] args) {
+		
+		// create the main frame
+		MainFrame mainFrame = new MainFrame();
+		mainFrame.setVisible(true);
+		
+		// create the Swimmer table
+		//new PerformDatabaseOperations();
+	
+		
+	}
+}
